7340c065b4f1b8d83b286af636ace09c8b7261ae
Guber <miralexsky@gmail.com>
2021-02-22 12:09:15 +0400

    решение для MeetupPageComponent

--- 02-components/02-MeetupPageComponent/MeetupAgenda.js
+++ 02-components/02-MeetupPageComponent/MeetupAgenda.js
@@ -1,14 +1,23 @@
-// import { MeetupAgendaItem } from './MeetupAgendaItem.js';
+import { MeetupAgendaItem } from './MeetupAgendaItem.js';
 
 export const MeetupAgenda = {
   name: 'MeetupAgenda',
 
   template: `
     <div class="meetup-agenda">
-      <div class="meetup-agenda__item"></div>
+      <div class="meetup-agenda__item" v-for="agendaItem in agenda" :key="agendaItem.id">
+        <meetup-agenda-item :agendaItem="agendaItem"/>
+      </div>
     </div>`,
 
-  // components
+  components: {
+    MeetupAgendaItem,
+  },
 
-  // props
+  props: {
+    agenda: {
+      required: true,
+      type: Array
+    }
+  }
 };
