3aaef1de532f7ec163e6dff43d474b51caaf7b64
Guber <miralexsky@gmail.com>
2021-02-25 20:37:52 +0400

    remove trash

--- 02-components/02-MeetupPageComponent/MeetupView.js
+++ 02-components/02-MeetupPageComponent/MeetupView.js
@@ -9,13 +9,6 @@ export const MeetupView = {
 
   template: `
     <div>
-      <component-slot>
-        <template #[pers1]="{ dialogue }"><div>{{ dialogue }}</div></template>
-        <template #[pers2]="slot">{{ slot.dialogue }}</template>
-      </component-slot>
-      <component-single-slot #default="{ dialogue, otherDi }">
-        <div>{{ dialogue + ' ' + otherDi }}</div>
-      </component-single-slot>
       <meetup-cover :link="coverLink" :title="meetup.title"/>
       <div class="container">
         <div class="meetup">
@@ -34,44 +27,22 @@ export const MeetupView = {
     </div>`,
 
   components: {
-    componentSlot: {
-      template: `<div>Garry: <slot name="garry" :dialogue="garryDialogue">Somthing magical here</slot> Ron: <slot name="ron" :dialogue="ronDialogue">Yes, you are right</slot></div>`,
-      props: {
-
-      },
-      data() {
-        return {
-          ronDialogue: 'I\'m redhead!!',
-          garryDialogue: 'You are redhead',
-        }
-      },
-    },
-    componentSingleSlot: {
-      template: `<div>Ron: <slot dialogue="I'm single!!!" otherDi="Fuck you Garry">Give me my rat!!</slot></div>`,
-    },
     MeetupCover,
     MeetupDescription,
     MeetupAgenda,
     MeetupInfo,
   },
 
-  data() {
-    return {
-      pers1: 'garry',
-      pers2: 'ron',
-    }
-  },
-
   props: {
     meetup: {
       required: true,
-      type: Object
-    }
+      type: Object,
+    },
   },
 
   computed: {
     coverLink() {
-      return getMeetupCoverLink(this.meetup)
+      return getMeetupCoverLink(this.meetup);
     },
-  }
+  },
 };
