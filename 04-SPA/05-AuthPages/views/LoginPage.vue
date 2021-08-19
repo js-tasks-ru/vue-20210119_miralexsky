3bbafe356658b69da48cbd0834c09445375cca0e
miralexsky <miralexsky@gmail.com>
2021-08-20 01:53:22 +0400

    решение для spa/05

--- 04-SPA/05-AuthPages/views/LoginPage.vue
+++ 04-SPA/05-AuthPages/views/LoginPage.vue
@@ -1,12 +1,63 @@
 <template>
-  <form class="form"></form>
+  <div>
+    <form class="form" @submit.prevent="auth">
+      <p>
+        <input type="text" placeholder="login" name="login" v-model="login">
+      </p>
+      <p>
+        <input type="text" placeholder="password" name="pass" v-model="pass">
+      </p>
+      <input type="submit" name="Submit">
+    </form>
+    <router-link to="/register">Зарегистрироваться</router-link>
+  </div>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login as loginApi } from '../data';
 
 export default {
   name: 'LoginPage',
+  data() {
+    return {
+      login: '',
+      pass: '',
+    }
+  },
+  methods: {
+    auth() {
+      let validated = this.validate();
+
+      if (validated.error) {
+        alert(validated.message)
+        return
+      }
+
+      let response = loginApi(this.login, this.pass);
+      response.then((data) => {
+        if (data.fullname) {
+          alert(data.fullname);
+        } else {
+          alert('Неверные учетные данные');
+        }
+      })
+    },
+
+    validate() {
+      let message = '';
+
+      if (!this.login) {
+        message = 'Требуется ввести Email'
+      } else if (!this.pass) {
+        message = 'Требуется ввести пароль'
+      }
+
+      return {
+        error: !!message,
+        message: message,
+      }
+    }
+  }
 };
 </script>
 
